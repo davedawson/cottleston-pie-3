8234165bbd6c1c6d054d07dc84c03f004cf2617d
Dave Dawson <davedawson@Daves-MBP.home>
2015-09-17 16:41:24 -0400

    Get gulp working for reals this time

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var compass        = require('gulp-compass');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -9,15 +10,20 @@ gulp.task('serve', ['sass'], function() {
         proxy: "cp.dev"
     });
 
-    gulp.watch("/sass/*.scss", ['sass']);
+    gulp.watch("sass/**/*.scss", ['sass']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("app/css"))
+    return gulp.src("sass/**/*.scss")
+		    .pipe(compass({
+		      config_file: './config.rb',
+		      css: 'stylesheets',
+		      sass: 'sass'
+		    }))
+        // .pipe(sass())
+        .pipe(gulp.dest("stylesheets"))
         .pipe(browserSync.stream());
 });
 
